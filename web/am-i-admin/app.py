ee7127655430eaf2445fbad7b0010fefd6ab404b
chase <cjopsahl99@gmail.com>
2025-04-07 15:39:11 -0500

    Remove --real

--- web/am-i-admin/app.py
+++ web/am-i-admin/app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/exec-suid --real -- /usr/bin/python -I
+#!/usr/bin/exec-suid -- /usr/bin/python -I
 
 from flask import Flask, render_template, make_response, request
 from secrets import token_hex
