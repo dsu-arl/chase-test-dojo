fd4ab49dd7b27214ca398bae23bbee980430a645
chase <cjopsahl99@gmail.com>
2025-04-07 14:19:02 -0500

    New Challenge

---
+++ web/am-i-admin/DESCRIPTION.md
@@ -0,0 +1,3 @@
+# Am I Admin
+
+Frequently, cookies are used to identify users and their access levels to applications. However, developers must remember that users can modify anything sent to the their system. Can you become an admin in this example app?
---
+++ web/am-i-admin/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, make_response, request
+from secrets import token_hex
+
+app = Flask(__name__)
+app.secret_key = token_hex(32)
+
+try:
+	with open('/flag', 'r') as fObj:
+		flag = fObj.read()
+except Exception as e:
+	print(e)
+	print("Failed to get flag from '/flag'!")
+
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template("login.html") # this page isn't functional, it's just presentation of a login page
+
+@app.route("/admin")
+def admin():
+    # check if user is admin from cookie
+    isadmin = request.cookies.get('isadmin', None)
+    if not isadmin or isadmin == "0":
+        isadmin = False
+        resp = make_response(render_template("admin.html", isadmin=isadmin))
+        resp.set_cookie("isadmin", "0")
+        return resp
+    else:
+        return render_template("admin.html", flag=flag, isadmin=isadmin)
